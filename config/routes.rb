d2a838d8f454b5bc78c115f32c101d4413e1e67b
Chris Walsh <chris.w894j@gmail.com>
2018-08-13 16:07:45 +0200

    routes changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
-  devise_for :users do 
+
+  devise_for :users do
   end
 
-  resources :bookings
-  resources :walls
+
+  get 'owner_bookings', to: 'bookings#owner_bookings', as: 'owner_bookings'
+  get 'artist_bookings', to: 'bookings#artist_bookings', as: 'artist_bookings'
+
+  resources :walls do
+    #
+    resources :bookings, only: [:index, :show, :create, :edit, :update]
+    # bookings show, gives booking/s related to specific walls
+    # bookings new, create new booking for specific wall
+    #form for new booking of wall is on wall/:id/show
+    #etc
+  end
+
+  resources :bookings, only: :destroy
+
+  ## when logged in, get all my walls as owner
+  get 'my_walls', to: 'my_walls#walls', as: 'my_walls'
 end
