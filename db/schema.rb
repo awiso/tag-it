6e99e6a324a4ad2c54b284d01e1ee19aeb322e84
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-15 12:19:25 +0200

    add maps to index and show, autocomplete too

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_143630) do
+ActiveRecord::Schema.define(version: 2018_08_15_084702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2018_08_14_143630) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_walls_on_user_id"
   end
 
