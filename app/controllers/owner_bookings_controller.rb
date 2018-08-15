375790d69a0161955e7fbeae8288d9f005813c1e
Steven Moore <steven.moore177@gmail.com>
2018-08-15 11:12:49 +0200

    implement back end of my_walls for owners

--- app/controllers/owner_bookings_controller.rb
+++ app/controllers/owner_bookings_controller.rb
@@ -5,6 +5,5 @@ class OwnerBookingsController < ApplicationController
       policy_scope(Booking, policy_scope_class: OwnerBookingsPolicy::Scope)
       @walls = current_user.owned_walls
 
-      end
   end
 end
