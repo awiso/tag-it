5c1c2b239cf322314f75adad2f122214e38ea856
Chris Walsh <chris.w894j@gmail.com>
2018-08-17 15:44:02 +0200

    styled the walls index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def active_class(link_path)
     current_page?(link_path) ? "active" : ""
   end
+
+  def navbar_class
+    if current_page? walls_path
+      return "fixed"
+    else
+      return ""
+    end
+  end
 end
